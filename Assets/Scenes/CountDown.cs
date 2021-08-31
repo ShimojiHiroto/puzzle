b25e4749365a90832006881681adf0b7ac973897
ShimojiHiroto <handoballsky@gmail.com>
2021-08-31 16:14:27 +0900

    完成

---
+++ Assets/Scenes/CountDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CountDown : MonoBehaviour
+{
+    private float time = 60.0f;
+    public Text timerText;
+    public bool isTimeUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTimeUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(0 < time)
+        {
+            time -= Time.deltaTime;
+            timerText.text = time.ToString("F1");
+        }
+        else if(time < 0)
+        {
+            isTimeUp = true;
+        }
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TouchManager.Ended += (info) => 
+        {
+            Debug.Log("押されたスクリーン座標は"　+ info.screenPoint); 
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
